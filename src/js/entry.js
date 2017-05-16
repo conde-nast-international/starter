5390a29b7186bc97117659d0bd4cbb9e72fa8b9b
Jacqui Maher <jacqui.maher@condenastint.com>
2017-05-16 16:54:13 +0100

    ScrollStory javascript

--- src/js/entry.js
+++ src/js/entry.js
@@ -4,23 +4,13 @@ import isMobile from './utils/is-mobile'
 import graphic from './graphic'
 
 const bodyEl = select('body')
-let previousWidth = 0
-
-function handleResize() {
-	const width = bodyEl.offsetWidth
-	if (previousWidth !== width) {
-		previousWidth = width
-		graphic.resize()
-	}
-}
 
 function init() {
 	// add mobile class to body tag
 	if (isMobile.any()) addClass(bodyEl, 'is-mobile')
-	// setup resize event
-	window.addEventListener('resize', debounce(handleResize, 150))
 	// kick off graphic code
 	graphic.init()
 }
 
 init()
+
--- src/js/graphic.js
+++ src/js/graphic.js
@@ -1,11 +1,54 @@
 import * as d3 from 'd3'
 
-function resize() {
-
+function scrollstory() {
+  // select elements using jQuery since it is a dependency
+  var $graphicEl = $('.graphic')
+  var $graphicVisEl = $graphicEl.find('.graphic__vis')
+  // viewport height
+  var viewportHeight = window.innerHeight
+  var halfViewportHeight = Math.floor(viewportHeight / 2)
+  // a global function creates and handles all the vis + updates
+  var graphic = createGraphic('.graphic')
+  // handle the fixed/static position of grahpic
+  var toggle = function(fixed, bottom) {
+    if (fixed) $graphicVisEl.addClass('is-fixed')
+    else $graphicVisEl.removeClass('is-fixed')
+    if (bottom) $graphicVisEl.addClass('is-bottom')
+    else $graphicVisEl.removeClass('is-bottom')
+  }
+  // callback function when scrollStory detects item to trigger
+  var handleItemFocus = function(event, item) {
+    var step = item.data.step
+    graphic.update(step)
+  }	
+  // callback on scrollStory scroll event
+  // toggle fixed position
+  var handleContainerScroll = function(event) {
+    var bottom = false
+    var fixed = false
+    var bb = $graphicEl[0].getBoundingClientRect()
+    var bottomFromTop = bb.bottom - viewportHeight
+    if (bb.top < 0 && bottomFromTop > 0) {
+      bottom = false
+      fixed = true
+    } else if (bb.top < 0 && bottomFromTop < 0) {
+      bottom = true
+      fixed = false
+    }
+    toggle(fixed, bottom)
+  }
+  // instantiate scrollStory
+  $graphicEl.scrollStory({
+    contentSelector: '.trigger',
+    triggerOffset: halfViewportHeight,
+    itemfocus: handleItemFocus,
+    containerscroll: handleContainerScroll,
+  })
 }
 
 function init() {
 	console.log('Make something awesome!')
+  scrollstory()
 }
 
-export default { init, resize }
+export default { init, scrollstory }
