ee9da32bd292a7e9a7e5b5f229603ddb19dfe72c
Jacqui Maher <jacqui.maher@condenastint.com>
2017-05-16 16:49:21 +0100

    Updated font classes and basic html for articles on international Vogue

--- src/js/critical.js
+++ src/js/critical.js
@@ -1,21 +1,14 @@
 import { loadFontGroup } from './utils/load-font'
 
-const canela = [
-	{ family: 'Canela Web', weight: 300 },
-	{ family: 'Canela Web', weight: 700 },
+const didot = [
+	{ family: 'GFS Didot', weight: 300 },
+	{ family: 'GFS Didot', weight: 700 },
 ]
 
-const publico = [
-	{ family: 'Publico Text Web', weight: 400 },
-	{ family: 'Publico Text Web', weight: 700 },
+const opensans = [
+	{ family: 'Open Sans', weight: 400 },
+	{ family: 'Open Sans', weight: 700 },
 ]
 
-const atlas = [
-	{ family: 'Atlas Grotesk Web', weight: 300 },
-	{ family: 'Atlas Grotesk Web', weight: 400 },
-	{ family: 'Atlas Grotesk Web', weight: 500 },
-]
-
-loadFontGroup(canela)
-loadFontGroup(publico)
-loadFontGroup(atlas)
+loadFontGroup(didot)
+loadFontGroup(opensans)
